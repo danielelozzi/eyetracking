6c1e82db15bf8d784cc9de12c08b87a52b363ce4
Daniele Lozzi <lozzi.dan@gmail.com>
2023-10-18 15:33:12 +0200

    fixed timestamp

--- script.bat
+++ script.bat
@@ -1,4 +1,4 @@
 cd C:/Program Files (x86)/SuperLab 4.0/
-superlab --expfile="C:/Users/TUTTI/Desktop/PROVA_EYE_TRACKER/PROVA_TASK_EYETRACKER.sl4" --nosplash --quit -o="C:/Users/TUTTI/Desktop/PROVA_EYE_TRACKER/daniele_3.txt" --group="eye_tracker" --name="daniele_3"
+superlab --expfile="C:/Users/TUTTI/Desktop/PROVA_EYE_TRACKER/PROVA_TASK_EYETRACKER.sl4" --nosplash --quit -o="C:/Users/TUTTI/Desktop/PROVA_EYE_TRACKER/matias_3.txt" --group="eye_tracker" --name="matias_3"
 exit
 
